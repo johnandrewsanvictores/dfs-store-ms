8586835d421057f4c08260169385e74959915c36
johnandrewsanvictores <sanvictoresjohnandrewe@gmail.com>
2025-01-11 21:15:35 +0800

    finished controls and table design

---
+++ js/staff.js
@@ -0,0 +1,67 @@
+
+var table = null;
+
+document.addEventListener("DOMContentLoaded", function() {
+    table = Table.initDataTable();
+    Table.addSelectEvent(table);
+
+});
+
+
+
+const Table = (function() {
+    function initDataTable() {
+        return new DataTable('#staff-table', {
+            scrollX: true,
+            responsive: true,
+            // processing: true,
+            // serverSide: true,
+            // ajax: {
+            //     url: "../api/staff_api.php",
+            //     type: "post",
+            //     // data: {'action' : "datatableDisplay", "selected_field" : select_field_node.value},
+            // },
+            // // ajax: "../../api/business_datatable_api.php",
+            // "columns": [
+            //     { "data": "id", visible: false },
+            //     { "data": "name" },
+            //     { "data": "field" },
+            //     { "data": "location" },
+            //     { "data": "contact_number" },
+            // ]
+        });
+    }
+
+
+    function addSelectEvent(table) {
+        table.on('click', 'tbody tr', function (e) {
+            e.currentTarget.classList.toggle('selected');
+        });
+    }
+
+    function getSelectedRow(table) {
+        return table.rows('.selected').data()
+    }
+
+    function removeRow(table) {
+        table.row('.selected').remove().draw(false);
+    }
+
+    function deselect_all_selected_row() {
+        document.querySelectorAll('tbody tr.selected').forEach(el => el.classList.remove('selected'));
+    }
+
+    function select_all_rows() {
+        document.querySelectorAll('tbody tr').forEach(row => row.classList.add('selected'));
+    }
+
+
+    return {
+        initDataTable,
+        addSelectEvent,
+        getSelectedRow,
+        removeRow,
+        deselect_all_selected_row,
+        select_all_rows
+    }
+})();
--- pages/staff.php
+++ pages/staff.php
@@ -6,19 +6,104 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Sidebar Menu for Admin Dashboard | CodingNepal</title>
+    <title>Dream Fashion Shop SMS | Staff Account Management</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.1.7/css/dataTables.dataTables.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
     <link rel="stylesheet" href="../styles/staff.css" />
     <link rel="stylesheet" href="../global.css">
-    <!-- Fontawesome CDN Link -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
 </head>
 
 <body>
     <?php include('../includes/navbar.php'); ?>
 
     <main class="main">
-        <h1>Staff Management Content</h1>
+
+        <div class="header-staff">
+            <h4>Staff Account Management</h4>
+            <div class="controls-staff">
+                <button id="staff-new-btn">
+                    <i class="fas fa-plus"></i>
+                    <span>New</span>
+                </button>
+                <button id="edit-btn">
+                    <i class="fas fa-pencil-alt"></i>
+                    <span>Edit</span>
+                </button>
+                <button id="remove-btn">
+                    <i class="fas fa-trash"></i>
+                    <span>Remove</span>
+                </button>
+            </div>
+        </div>
+
+        <div class="table-div">
+            <table id="staff-table" class="display stripe" style="width:100%">
+                <thead>
+                    <tr>
+                        <th>Staff Id</th>
+                        <th>Name</th>
+                        <th>Username</th>
+                        <th>Phone Number </th>
+                        <th>Role</th>
+                        <th>Date Added</th>
+                        <th>Last Login</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <tr>
+                        <td>STF001</td>
+                        <td>John Andrew San Victores</td>
+                        <td>Drew</td>
+                        <td>09167003378</td>
+                        <td>Admin</td>
+                        <td>09-12-24</td>
+                        <td>2025-01-11 15:30:45</td>
+                    </tr>
+
+                    <tr>
+                        <td>STF002</td>
+                        <td>Andrew San Victores</td>
+                        <td>Drew</td>
+                        <td>09167003378</td>
+                        <td>Admin</td>
+                        <td>09-12-24</td>
+                        <td>2025-01-11 15:30:45</td>
+                    </tr>
+
+                    <tr>
+                        <td>STF003</td>
+                        <td>John </td>
+                        <td>Drew</td>
+                        <td>09167003378</td>
+                        <td>Cashier</td>
+                        <td>09-12-24</td>
+                        <td>2025-01-11 15:30:45</td>
+                    </tr>
+
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <th>Staff Id</th>
+                        <th>Name</th>
+                        <th>Username</th>
+                        <th>Phone Number </th>
+                        <th>Role</th>
+                        <th>Date Added</th>
+                        <th>Last Login</th>
+                    </tr>
+                </tfoot>
+            </table>
+        </div>
     </main>
+
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <script src="https://cdn.datatables.net/2.1.7/js/dataTables.js"></script>
+
+    <script src="../js/staff.js"></script>
+
 </body>
 
 </html>
