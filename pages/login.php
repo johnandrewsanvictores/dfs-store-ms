e9d1e9a767b9fcc0dfa1070c4d9d64d3a6c3249a
johnandrewsanvictores <sanvictoresjohnandrewe@gmail.com>
2025-01-30 03:44:38 +0800

    login design

--- pages/login.php
+++ pages/login.php
@@ -5,21 +5,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dream Fashion Shop - Login</title>
-    meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
     <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&family=Lobster&display=swap" rel="stylesheet">
-
     <link rel="stylesheet" href="../global.css">
-
+    <link rel="stylesheet" href="../styles/login.css">
 </head>
 
 <body>
     <?php require("../includes/navbar.php") ?>
+
+    <div class="login-page">
+        <div class="login-image">
+            <img src="../assets/images/shopping.svg" alt="E-commerce Image">
+        </div>
+        <div class="login-form">
+            <h2>Login</h2>
+            <form action="login_process.php" method="post">
+                <input type="text" name="username" placeholder="Username" required>
+                <input type="password" name="password" placeholder="Password" required>
+                <button type="submit">Login</button>
+                <a href="forgot_password.php" class="forgot-password">Forgot Password?</a>
+                <p class="register-link">Don't have an account? <a href="register.php">Register here</a></p>
+            </form>
+        </div>
+    </div>
 </body>
 
 </html>
